85e6677d76d6194d790256f8bbec3205b57ec4a0
Serhii Rubtsov <rubtsov1986@bigmir.net>
2022-04-10 12:44:55 +0300

    bugfix

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,5 +1,4 @@
 import s from './Container.module.css';
-import PropTypes from 'prop-types';
 
 export default function Container({children}) {
     return <div className={s.container}>{children }</div>
